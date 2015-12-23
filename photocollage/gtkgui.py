6d3f1d46f90d488fa5f8e42995b0dda8d61a1858
Pavel Grunt <pgrunt@redhat.com>
2015-12-23 20:23:58 +0100

    gtkgui: Use gi.require_version
    Get rid of a runtime warning

--- photocollage/gtkgui.py
+++ photocollage/gtkgui.py
@@ -20,6 +20,8 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 import cairo
 import copy
 import gettext
+import gi
+gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk, GObject
 from io import BytesIO
 import math
