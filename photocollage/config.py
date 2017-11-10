f15770d447697eec77b69cb7262b9575ce8fb961
Joël BOURGAULT <jbourgault@sii.fr>
2017-11-10 10:14:57 +0100

    #38: trying again to pass flake8-import-order check
    Signed-off-by: Joël BOURGAULT <jbourgault@sii.fr>

--- photocollage/config.py
+++ photocollage/config.py
@@ -15,13 +15,10 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-
-
 import gettext
 import logging
 import os
 import time
-
 import yaml
 
 from photocollage import APP_NAME
