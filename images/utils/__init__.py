dbca388c3eeff6c6f687188fb9719848b7521edd
Anuj <foranuj@yahoo.com>
2021-12-22 17:33:06 -0800

    Removing tensorflow

--- images/utils/__init__.py
+++ images/utils/__init__.py
@@ -1,32 +1,11 @@
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.resnet50 import preprocess_input
-import numpy as np
-
-
-def generate_vector_from_img(image_file, model):
-    """
-    Given an image this generated a feature vector for that image
-    :param image_file: An image, for which a feature vector can be generated
-    :return: A feature vector associated with that image.
-    """
-
-    print("creating vector for image", image_file)
-    img = image.load_img(image_file, target_size=(224, 224))
-
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-
-    x = preprocess_input(x)
-
-    return image_file, model.predict(x)
-
-
 def get_orientation_fixed_pixbuf(img: str):
-    import PIL, gi
+    import PIL
+    import gi
     gi.require_version('Gtk', '3.0')
     gi.require_version('GdkPixbuf', '2.0')
 
     from gi.repository import GdkPixbuf
+    pixbuf = None
 
     try:
         pil_image = PIL.Image.open(img)
