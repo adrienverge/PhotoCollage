3289328d09dee6d19895ef013633c1c107ff41d5
Adrien Vergé <adrienverge@gmail.com>
2025-07-09 06:10:48 +0200

    PhotoCollage 1.5.0

--- photocollage/__init__.py
+++ photocollage/__init__.py
@@ -15,7 +15,7 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 APP_NAME = "photocollage"
-APP_VERSION = "1.4.8"
+APP_VERSION = "1.5.0"
 
 __author__ = "Adrien Vergé"
 __copyright__ = "Copyright 2013, Adrien Vergé"
